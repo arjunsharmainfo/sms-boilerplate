b6d6cd124aa51ade5b2585d6c5bdfd4c5f53f8b4
sreekanth <sreekanth.kolamala@gmail.com>
2020-10-06 00:01:55 +0800

    initail boilerplate

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var cors = require('cors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var app = express();
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3000);
+
+app.get('/', (req, res) => {
+  res.json(200);
+});
